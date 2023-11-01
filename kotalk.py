690bcad159978bfe828d9c377a040c06206eb30b
tr-mumford <tyler.r.mumford@gmail.com>
2023-11-01 22:21:52 +0200

    Sketched out 10% of app structure.

--- kotalk.py
+++ kotalk.py
@@ -1,4 +1,14 @@
 import classTest
 
+# Connect to client
+    # On startup broadcast LAN signal
+    # Show LAN signals (default names) 
+    # If succeed first time, then can send to buffer + client
+        # Else try again please
+
+# Get and store user input
+    # Read user keystrokes
+# Connect to 
+
 HelloObject = classTest.HelloClass()
 HelloObject.cprint()
---
+++ network_classes.py
@@ -0,0 +1,7 @@
+class ConnectionDisplay:
+    wip = "wip"
+    # Print
+        # Your default name
+        # LAN connections numbered
+    # Input
+        # Wait to get LAN connection number
